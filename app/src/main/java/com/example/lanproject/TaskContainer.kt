67d233fe7bae9da442977ea0a1a933686afe1af6
Linus W. Monie <zerox7felf@gmail.com>
2020-12-10 16:49:16 +0100

    TaskContainer added

---
+++ app/src/main/java/com/example/lanproject/TaskContainer.kt
@@ -0,0 +1,46 @@
+package com.example.lanproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class TaskContainer : AppCompatActivity() {
+    private inner class PageAdapter(activity: TaskContainer, private val pages: List<Fragment>) : FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int = pages.count()
+        override fun createFragment(position: Int): Fragment = pages[position]
+    }
+
+    // TODO: Fan, this is quick and dirty
+    // Fragments for Tasks 1-5
+    private val taskFragments = listOf(
+        TaskHelpPlaceholder(),
+        TaskHelpPlaceholder(),
+        TaskHelpPlaceholder(),
+        TaskHelpPlaceholder(),
+        TaskHelpPlaceholder()
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_container)
+
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        viewPager.adapter = PageAdapter(this, listOf(
+            taskFragments[intent.getIntExtra("taskID", 0)], // Task Fragment
+            TaskHelpPlaceholder()                                             // Help Fragment
+        ))
+
+        val tabTexts = listOf("Task", "Learning Materials")
+        TabLayoutMediator(  // TODO: prettify all this
+            findViewById(R.id.tabLayout),
+            viewPager
+        ) {
+            tab: TabLayout.Tab, position: Int ->
+            tab.text = if (position <= 1) tabTexts[position] else "Naj nu vart de fel"
+        }.attach()
+    }
+}
---
+++ app/src/main/java/com/example/lanproject/TaskHelpPlaceholder.kt
@@ -0,0 +1,13 @@
+package com.example.lanproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TaskHelpPlaceholder : Fragment(R.layout.fragment_task_help_placeholder) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
