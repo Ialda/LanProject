0df2001f4d25388f7609939bd7fd849d44550712
azinum <lukaska61@gmail.com>
2020-12-15 16:44:18 +0100

    Temporarily inflate gap spinner on cardview

--- app/src/main/java/com/example/lanproject/TaskFragment1.kt
+++ app/src/main/java/com/example/lanproject/TaskFragment1.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.fragment_task_help_placeholder.view.*
+import kotlinx.android.synthetic.main.fragment_task1.*
+import kotlinx.android.synthetic.main.gap_spinner.*
 
 class TaskFragment1 : Fragment(R.layout.fragment_task1) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
+        val view = LayoutInflater.from(cardView5.context)
+        val spinner = view.inflate(R.layout.gap_spinner, null)
     }
 }
