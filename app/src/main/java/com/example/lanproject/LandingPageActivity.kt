099d839e7ef70bb6813e7784d027115b4bf6219f
Lucas Larsson <lln19011@student.mdh.se>
2020-12-20 23:41:00 +0100

    Landing page dynamically creates logs. Needs work to function with TaskResult (typeMismatch).

--- app/src/main/java/com/example/lanproject/LandingPageActivity.kt
+++ app/src/main/java/com/example/lanproject/LandingPageActivity.kt
@@ -1,10 +1,13 @@
 package com.example.lanproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setMargins
+import java.util.*
 
 class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,73 @@ class LandingPageActivity : AppCompatActivity() {
         startActivity(Intent(this, MainPageActivity::class.java))
     }
 
-    fun Logout(view: View) {
-        finish()
+    fun CreateNewHistory(view: View) {
+        val HistoryView = findViewById<LinearLayout>(R.id.LinearLayoutRes)
+        val HorView = LinearLayout(this)
+        val LinearView = LinearLayout(this)
+        val LinearViewRes = LinearLayout(this)
+
+        val TextViewAct = TextView(this)
+        val TextViewPoint = TextView(this)
+        val TextViewDiff = TextView(this)
+        val TextViewDate = TextView(this)
+        val TextViewActRes = TextView(this)
+        val TextViewPointRes = TextView(this)
+        val TextViewDiffRes = TextView(this)
+        val TextViewDateRes = TextView(this)
+
+        val LinearParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        LinearParams.setMargins(25)
+        LinearView.orientation = LinearLayout.VERTICAL
+        LinearViewRes.orientation = LinearLayout.VERTICAL
+
+        //testing area
+        /*val ResultList = listOf(int, int, int, date)
+        ResultList[0].result = 3
+        ResultList[0].maxResult = 10
+        ResultList[0].difficulty = 1
+        ResultList[0].timeStamp = Date()*/
+        //end of area
+
+        TextViewAct.text = getString(R.string.activity)
+        TextViewPoint.text = getString(R.string.points)
+        TextViewDiff.text = getString(R.string.difficulty)
+        TextViewDate.text = getString(R.string.date)
+        /*var DiffRes = "placeholder"
+        when (ResultList[0].difficulty) {
+            0 -> "Easy"
+            1 -> "Medium"
+            2 -> "Hard"
+        }*/
+        val Point = "3"
+        val MaxPoint = "10"
+
+        TextViewActRes.text = "Reading"
+        TextViewPointRes.text = "$Point/$MaxPoint"
+        TextViewDiffRes.text = "Medium"
+        TextViewDateRes.text = Date().toString()
+
+        TextViewAct.textSize = 24f
+        TextViewPoint.textSize = 24f
+        TextViewDiff.textSize = 24f
+        TextViewDate.textSize = 24f
+        TextViewActRes.textSize = 24f
+        TextViewPointRes.textSize = 24f
+        TextViewDiffRes.textSize = 24f
+        TextViewDateRes.textSize = 24f
+
+        LinearView.addView(TextViewAct)
+        LinearView.addView(TextViewPoint)
+        LinearView.addView(TextViewDiff)
+        LinearView.addView(TextViewDate)
+        LinearViewRes.addView(TextViewActRes)
+        LinearViewRes.addView(TextViewPointRes)
+        LinearViewRes.addView(TextViewDiffRes)
+        LinearViewRes.addView(TextViewDateRes)
+
+        HorView.addView(LinearView, LinearParams)
+        HorView.addView(LinearViewRes, LinearParams)
+
+        HistoryView.addView(HorView)
     }
 }
