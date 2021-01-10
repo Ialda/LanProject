c39e0f115702b55b2682df4613cc92b95af6bffa
segerstorm <asm12005@student.mdh.se>
2021-01-10 12:45:11 +0100

    Uppdaterade textboxarna i Login och Register med material design implementation. Register knappen på register sidan pekar också tillbaka till login nu.

--- app/src/main/java/com/example/lanproject/RegisterPageActivity.kt
+++ app/src/main/java/com/example/lanproject/RegisterPageActivity.kt
@@ -1,11 +1,19 @@
 package com.example.lanproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegisterPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
     }
+
+    fun registerPageView(view: View) {
+
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
 }
