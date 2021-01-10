93bae021bf68df792b99f3617fd9d956a6c64eca
segerstorm <asm12005@student.mdh.se>
2021-01-10 21:25:08 +0100

    Kopplar RegisterPageActivity.kt till databasen och felhanterar om Username är taget och om connectionen inte går.

--- app/src/main/java/com/example/lanproject/RegisterPageActivity.kt
+++ app/src/main/java/com/example/lanproject/RegisterPageActivity.kt
@@ -5,15 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Base64
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Spinner
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_register_page.*
 
 class RegisterPageActivity : AppCompatActivity() {
+
+    var Username:EditText? = null
+    var Password:EditText? = null
+    var ConfirmPassword:EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
+        Username = findViewById<EditText>(R.id.PlainTextRegisterUsername)
+        Password = findViewById<EditText>(R.id.PasswordRegisterpswd)
+        ConfirmPassword = findViewById<EditText>(R.id.PasswordReRegisterpswd)
 
         PlainTextRegisterUsername.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
@@ -54,25 +69,60 @@ class RegisterPageActivity : AppCompatActivity() {
 
     fun registerPageView(view: View) {
 
-        val Username = findViewById<EditText>(R.id.PlainTextRegisterUsername)
-        val Password = findViewById<EditText>(R.id.PasswordRegisterpswd)
-        val ConfirmPassword = findViewById<EditText>(R.id.PasswordReRegisterpswd)
+        if (Username?.text.toString() == "" || Password?.text.toString() == "" || ConfirmPassword?.text.toString() == "") {
 
-        if (Username.text.toString() == "" || Password.text.toString() == "" || ConfirmPassword.text.toString() == "") {
-
-            if (Username.text.toString() == ""){
+            if (Username?.text.toString() == ""){
                 UsernameField.error = getString(R.string.ReqFieldError)
             }
-            if( Password.text.toString() == ""){
+            if( Password?.text.toString() == ""){
                 PasswordField.error = getString(R.string.ReqFieldError)
             }
-            if (ConfirmPassword.text.toString() == ""){
+            if (ConfirmPassword?.text.toString() == ""){
                 ConfirmPasswordField.error = getString(R.string.ReqFieldError)
             }
         }
-
+        else if (Password?.text.toString() != ConfirmPassword?.text.toString()) {
+            PasswordField.error = getString(R.string.PasswordMatchError)
+            ConfirmPasswordField.error = getString(R.string.PasswordMatchError)
+        }
         else
-        startActivity(Intent(this, MainActivity::class.java))
+            CreateUser()
+    }
+
+    fun CreateUser(){
+
+        val roleSpinner = findViewById<Spinner>(R.id.RoleSpinner)
+
+        var url: String? = null
+        val queue = Volley.newRequestQueue(this)
+
+        //url = "https.//lwm.sh/~lanproject/register.php?uanme=<" + Username?.text + ">&pass=<" + Password?.text + ">&role=<" + roleSpinner?.selectedItem.toString() + ">"
+        url = "https://lwm.sh/~lanproject/register.php?uname=" + Username?.text + "&pass=" + Password?.text + "&role=" + roleSpinner?.selectedItem.toString()
+        //https.//lwm.sh/~lanproject/register.php?uanme=<användarnamn>&pass=<pass>&role=<Student eller Teacher>
+
+        val stringRequest = StringRequest(Request.Method.GET, url, {
+                response ->
+            //Log.e("response", "YAS")
+            startActivity(Intent(this, MainActivity::class.java))
+
+        }, { error -> Log.e("response", "nei...")
+            //Log.e("res", error.toString())
+            //Log.e("res", url)
+            //ConfirmPasswordField.error = error.toString()
+            //if (error.toString() == "com.android.volley.NoConnectionError:")
+            //    UsernameField.error = error.toString()
+            //else if (error.toString() == "com.android.volley.)
+            if (error.toString() == "com.android.volley.ClientError")
+                UsernameField.error = getString(R.string.UsernameTaken)
+            else {
+                UsernameField.error = getString(R.string.ConnectFail)
+                PasswordField.error = getString(R.string.ConnectFail)
+                ConfirmPasswordField.error = getString(R.string.ConnectFail)
+            }
+        })
+
+        queue.add(stringRequest)
+        //startActivity(Intent(this, MainActivity::class.java))
     }
 
 }
