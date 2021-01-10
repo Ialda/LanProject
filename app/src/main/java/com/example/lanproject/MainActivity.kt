98bc215c05094665cd9c86ca7d1d9bd893b272ea
segerstorm <asm12005@student.mdh.se>
2021-01-10 17:00:54 +0100

    Felhantering för tomma fält. Nu kravs att rutorna inehåller något för att man ska kunna logga in.

--- app/src/main/java/com/example/lanproject/MainActivity.kt
+++ app/src/main/java/com/example/lanproject/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.lanproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.PasswordAuthentication
 
@@ -16,15 +19,52 @@ class MainActivity : AppCompatActivity() {
         ButtonLogin.setOnClickListener { view ->
             MainPageView(view)
         }
+        /*PasswordUserpswd.setOnFocusChangeListener{ view, hasfocus ->
+            if (hasfocus)
+                PasswordTextlayout.setError(null)
+        }*/
+
+        PlainTextUsername.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                UsernameField.setError(null)
+            }
+        })
+
+        PasswordUserpswd.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                PasswordField.setError(null)
+            }
+        })
     }
 
     fun MainPageView(view: View) {
         val Username = findViewById<EditText>(R.id.PlainTextUsername)
         val Password = findViewById<EditText>(R.id.PasswordUserpswd)
 
-        if (Username.text.toString() == "" && Password.text.toString() == "") {
-            startActivity(Intent(this, MainPageActivity::class.java))
+        if (Username.text.toString() == "" || Password.text.toString() == "") {
+
+            if (Username.text.toString() == ""){
+                UsernameField.error = getString(R.string.ReqFieldError)
+            }
+            if( Password.text.toString() == ""){
+                PasswordField.error = getString(R.string.ReqFieldError)
+            }
         }
+
+        else
+            startActivity(Intent(this, MainPageActivity::class.java))
     }
 
     fun RegisterPageview(view: View) {
--- app/src/main/java/com/example/lanproject/RegisterPageActivity.kt
+++ app/src/main/java/com/example/lanproject/RegisterPageActivity.kt
@@ -3,16 +3,75 @@ package com.example.lanproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_register_page.*
 
 class RegisterPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
+        PlainTextRegisterUsername.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                UsernameField.setError(null)
+            }
+        })
+
+        PasswordRegisterpswd.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                PasswordField.setError(null)
+            }
+        })
+        PasswordReRegisterpswd.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                ConfirmPasswordField.setError(null)
+            }
+        })
+
     }
 
     fun registerPageView(view: View) {
 
+        val Username = findViewById<EditText>(R.id.PlainTextRegisterUsername)
+        val Password = findViewById<EditText>(R.id.PasswordRegisterpswd)
+        val ConfirmPassword = findViewById<EditText>(R.id.PasswordReRegisterpswd)
+
+        if (Username.text.toString() == "" || Password.text.toString() == "" || ConfirmPassword.text.toString() == "") {
+
+            if (Username.text.toString() == ""){
+                UsernameField.error = getString(R.string.ReqFieldError)
+            }
+            if( Password.text.toString() == ""){
+                PasswordField.error = getString(R.string.ReqFieldError)
+            }
+            if (ConfirmPassword.text.toString() == ""){
+                ConfirmPasswordField.error = getString(R.string.ReqFieldError)
+            }
+        }
+
+        else
         startActivity(Intent(this, MainActivity::class.java))
     }
 
